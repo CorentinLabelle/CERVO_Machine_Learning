6530e25e6f57e741b5343e92639641a771f5b4f5
CorentinLabelle <corentinlabelle2@gmail.com>
2024-07-29 14:09:33 -0400

    refactoring

--- training/training_input.py
+++ training/training_input.py
@@ -1,10 +1,10 @@
 import configparser
 import os
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List
-
 import numpy as np
 
+from training.trainings import TrainingType
 from models.linear_models.linear_models import PipelineFactory, BaseLinearPipeline
 from util import read_mat, abstract_input
 
@@ -15,16 +15,17 @@ class TrainingInput(abstract_input.AbstractInput):
 
     protocol_directory: str = None
     protocol_name: str = "default_protocol_name"
-    subjects: List[str] = None
-    condition: List[str] = None
+    subjects_to_keep: List[str] = None
+    subjects_to_skip: List[str] = None
+    train_studies: List[str] = None
+    test_studies: List[str] = None
 
-    class_folders: List[tuple] = None
-    class_folders_test: List[tuple] = None
     start_sample: int = 0
     end_sample: int = None
-    test_size: float = 0.2
-    n_splits: int = 5
-    random_state: int = None
+
+    sampling_frequency: int = None
+    high_freq: float = None
+    low_freq: float = None
 
     run_ridge: bool = False
     run_logistic: bool = False
@@ -37,16 +38,18 @@ class TrainingInput(abstract_input.AbstractInput):
     run_time_sampling: bool = False
     run_grid_search: bool = False
     nb_replications: int = 2
-
-    sampling_frequency: int = None
-    high_freq: float = None
-    low_freq: float = None
+    test_size: float = 0.2
+    n_splits: int = 5
+    random_state: int = None
 
     run_permutation: bool = False
     n_permutations: int = None
     permutation_n_splits: int = None
 
     def validate(self):
+
+        if self.subjects_to_keep is None:
+            self.subjects_to_keep = ["*"]
 
         if not isinstance(self.output_folder, str):
             raise Exception(f"'output_folder' has to be a string.")
@@ -57,13 +60,13 @@ class TrainingInput(abstract_input.AbstractInput):
         if not os.path.isdir(self.protocol_directory):
             raise Exception(f"'protocol_directory' does not exists: {self.protocol_directory}.")
 
-        if not isinstance(self.subjects, list):
-            raise Exception(f"'subjects' has to be a list.")
+        if not isinstance(self.subjects_to_keep, list):
+            raise Exception(f"'subject_patterns_to_keep' has to be a list.")
 
-        if not (self.class_folders is None or isinstance(self.class_folders, list)):
+        if not (self.train_studies is None or isinstance(self.train_studies, list)):
             raise Exception(f"'class_folders' has to be a list or None.")
 
-        if not (self.class_folders_test is None or isinstance(self.class_folders_test, list)):
+        if not (self.test_studies is None or isinstance(self.test_studies, list)):
             raise Exception("'class_folders_test' has to be a list or None.")
 
         if not (self.start_sample is None or isinstance(self.start_sample, int)):
@@ -72,24 +75,32 @@ class TrainingInput(abstract_input.AbstractInput):
         if not (self.end_sample is None or isinstance(self.end_sample, int)):
             raise Exception("End sample has to be an integer or None.")
 
+        if self.pre_processing is None:
+            raise Exception(f"'pre_processing' cannot be empty.")
+
         possible_pre_processing = ("AVERAGE", "VECTORIZE")
         if not (self.pre_processing.upper() in possible_pre_processing):
             raise Exception(f"'pre_processing' has to be one of these values: {possible_pre_processing}")
-
-        if self.condition is None:
-            self.condition = [""]
 
     def create_pipelines(self) -> List[BaseLinearPipeline]:
         pipelines = []
         if self.run_ridge:
             pipelines.append(PipelineFactory.create(pipeline_type="RIDGE", pre_processing=self.pre_processing))
-        elif self.run_logistic:
+        if self.run_logistic:
             pipelines.append(PipelineFactory.create(pipeline_type="LOGISTIC", pre_processing=self.pre_processing))
-        elif self.run_perceptron:
+        if self.run_perceptron:
             pipelines.append(PipelineFactory.create(pipeline_type="PERCEPTRON", pre_processing=self.pre_processing))
-        elif self.run_svc:
+        if self.run_svc:
             pipelines.append(PipelineFactory.create(pipeline_type="SVC", pre_processing=self.pre_processing))
         return pipelines
+
+    def get_training_types(self) -> List[TrainingType]:
+        training_types = list()
+        if self.run_cross_validation:
+            training_types.append(TrainingType.CROSS_VALIDATION)
+        if self.run_time_generalization:
+            training_types.append(TrainingType.TIME_GENERALIZATION)
+        return training_types
 
     def condition_to_string(self) -> str:
         if self.condition is None:
@@ -103,7 +114,7 @@ class TrainingInput(abstract_input.AbstractInput):
 
 
 if __name__ == "__main__":
-    cfg_file = "/training/training_input_template/as_config.ini"
+    cfg_file = "/training/training_input_template/as_training.ini"
     mat_file = "../ici.mat"
     ti_1 = TrainingInput.read(cfg_file)
     ti_2 = TrainingInput.read(mat_file)
